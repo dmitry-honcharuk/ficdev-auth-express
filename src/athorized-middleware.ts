82ba7a7be69f40156ddd8e64200c7b4350bfcdec
Dmitry Honcharuk <dmitry.honcharuk@gmail.com>
2021-08-05 00:26:37 +0300

    added header token lookup

--- src/athorized-middleware.ts
+++ src/athorized-middleware.ts
@@ -7,26 +7,33 @@ interface Settings {
   clientId: string;
   authUrlBase: string;
   cookieName?: string;
+  tokenHeaderName?: string;
 }
 
 interface ExtendedRequest extends Request {
   user?: User;
 }
 
-export function authorizedMiddlewareFactory({ clientId, authUrlBase, cookieName }: Settings) {
+export function authorizedMiddlewareFactory({
+  clientId,
+  authUrlBase,
+  cookieName,
+  tokenHeaderName = 'x-ficdev-auth-token',
+}: Settings) {
   return () => (req: ExtendedRequest, res: Response, next: NextFunction) => {
     const cookieService = cookieServiceFactory({ cookieName });
     const authService = authServiceFactory({ clientId, authUrlBase });
 
-    const token = cookieService.getToken(req, res);
+    const cookieToken = cookieService.getToken(req, res);
+    const headerToken = req.header(tokenHeaderName);
 
-    if (!token) {
+    if (!cookieToken && !headerToken) {
       res.status(401).end();
       return;
     }
 
     authService
-      .getUserDetailsByToken(token)
+      .getUserDetailsByToken((cookieToken ?? headerToken) as string)
       .then((user) => {
         if (!user) {
           res.status(401).end();
